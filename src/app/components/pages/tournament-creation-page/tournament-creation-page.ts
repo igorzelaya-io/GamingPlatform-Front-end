061f68f873c49cf549525b77a5a43d73667b45ec
Igor Zelaya <igorzelaya@Igors-MacBook-Pro-2.local>
2021-02-02 05:47:11 -0600

    Updated Source code

--- src/app/components/pages/tournament-creation-page/tournament-creation-page.ts
+++ src/app/components/pages/tournament-creation-page/tournament-creation-page.ts
@@ -3,7 +3,8 @@ import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { User } from '../../../models/user/user';
 import { Tournament } from '../../../models/tournament/tournament';
 import { Router, ActivatedRoute } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tournament-creation-page',
@@ -16,32 +17,39 @@ export class TournamentCreationPageComponent implements OnInit {
   txtDescription: FormControl;
   //txtLimitNumberOfTeams: number;
   txtPlatforms: FormControl;
+  tournamentTimeZone: FormControl;
   //tournamentTeamSize: TournamentTeamSize;
   tournamentModerator: User;
+  
   tournamentDate: Date;
-  tournamentRegion: FormControl;
+  tournamentCountry: FormControl;
   
   @ViewChild('tournamentDateTimeElement')
   tournamentDateElement: ElementRef;
 
   entryAndPriceForm: FormGroup;
+
+  countryInfo: any[] = [];
+  countriesUrl: string = 'https://raw.githubusercontent.com/sagarshirbhate/Country-State-City-Database/master/Contries.json'; 
  
   isSuccessfulTournamentInformationSubmission = false;
   isClicked = false;
   tournament: Tournament;
 
   constructor(private router: Router,
+			  private httpClient: HttpClient,
 			  private route: ActivatedRoute,
 			  @Inject(FormBuilder) private builder: FormBuilder) {
     this.txtName = new FormControl();
     this.txtPlatforms = new FormControl();
-	this.tournamentRegion = new FormControl();
+	this.tournamentCountry = new FormControl();
 	this.txtDescription = new FormControl();
     this.tournament = new Tournament();
 	this.tournamentModerator = new User();
+	this.tournamentTimeZone = new FormControl();
     this.entryAndPriceForm = this.builder.group({
-		entryFee: new FormControl(),
-		cashPrize: new FormControl()	
+		cashPrize: new FormControl(),	
+		entryFee: new FormControl()
 	});
   }
 
@@ -57,9 +65,31 @@ export class TournamentCreationPageComponent implements OnInit {
   	this.route.queryParams
 	.subscribe((params: User) => {
 		this.tournamentModerator = params['user'];
+	},
+	(err: any) => {
+		console.error(err);
 	});
-  	this.tournament.tournamentModerator = this.tournamentModerator;
+  	this.getCountries();
+   }
+
+  getAllCountries(): Observable<any>{
+	return this.httpClient.get<any>(this.countriesUrl);	
   }
+
+  getCountries(){
+	this.getAllCountries()
+	.subscribe((data: any) => {
+		console.log(data);
+		this.countryInfo = data.Countries;
+	},
+	(err: any) => {
+		console.error(err);
+	});
+  }
+
+  updateCountry(event: any){
+	this.tournamentCountry = event.target.value;
+  }  
 
   clickedButton(): void{
     this.isClicked = true;
@@ -70,10 +100,10 @@ export class TournamentCreationPageComponent implements OnInit {
 	this.tournament.tournamentDescription = this.txtDescription.value;
 	this.tournament.tournamentCashPrice = this.cashPrice.value;
 	this.tournament.tournamentEntryFee = this.entryFee.value;
-	if(this.txtName.valid && this.txtDescription.valid && this.cashPrice.valid && this.entryFee.valid){
-		this.router.navigate(['/tournament-creation-config'], {queryParams: {tournament: this.tournament}}); 	
-	}
-	return; 
+	this.tournament.tournamentRegion = this.tournamentCountry.value;
+	//this.tournament.tournamentDate = this.tournamentDate.value;
+	this.tournament.tournamentModerator = this.tournamentModerator;
+	this.router.navigate(['/tournament-creation-config'], {queryParams: { tournament: this.tournament}}); 
   }
 
 }
